82bd38945d15efc239a60ec6b386c0ef1aa6b457
minexin <378171315@qq.com>
2018-01-02 17:09:49 +0800

    OK

--- kern/monitor.c
+++ kern/monitor.c
@@ -59,7 +59,20 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
-	return 0;
+    struct Eipdebuginfo info;
+    uint32_t *ebp = (uint32_t *) read_ebp();
+    cprintf("Stack backtrace:\n");
+    while (ebp) {
+        cprintf(" ebp %08x eip %08x args ", ebp, ebp[1]);
+        for (int j = 2; j != 7; ++j) {
+            cprintf(" %08x", ebp[j]);   
+        }
+        cprintf("\n");
+        debuginfo_eip(ebp[1],&info);
+        cprintf("%s:%d: %.*s+%d\n",info.eip_file,info.eip_line,info.eip_fn_namelen,info.eip_fn_name,ebp[1]-info.eip_fn_addr);
+        ebp = (uint32_t *) (*ebp);
+    }
+   	return 0;
 }
 
 
